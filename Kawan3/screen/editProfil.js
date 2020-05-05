9980d8f4769561996badae3adbc59dd2d2476218
H34dr1k <hendrikjak17@gmail.com>
2020-05-05 14:38:09 +0700

    button

--- Kawan3/screen/editProfil.js
+++ Kawan3/screen/editProfil.js
@@ -30,7 +30,7 @@ class editProfil extends React.Component {
             )
         }else{
             return(
-                <View style={{flex: 0}}>
+                <View style={{flex: 1}}>
                     <View style={{flex: 0, backgroundColor: '#49438D', height: 229, paddingTop: 26.5}}>
                         <ImageBackground source={require('../src/image/decoStar.png')} style={{width: '100%', height: '100%', flexDirection: 'row'}}>
                         <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
@@ -54,6 +54,13 @@ class editProfil extends React.Component {
                             <TextInput placeholder="What will you do?" style={{height: 41, borderBottomWidth: 1, color: 'gray'}} />
                         </View>
                     </View>
+
+                    <TouchableOpacity style={{ flex: 1, marginLeft:170, marginTop:80}}>
+                        <View style={{ width: 80, padding: 20, height: 20, justifyContent: "center", alignContent: "center", backgroundColor: "blue" }}>
+                            <Text style={{ color: "white", textAlign:"center" }}>OK</Text>
+                        </View>
+                    </TouchableOpacity>
+                    
                 </View>
             );
         }
